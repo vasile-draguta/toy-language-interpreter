ec7491e1d0085a366c4fea7ebadcd939a82a0405
Vasile Draguța <dragutavasile@gmail.com>
2024-10-28 13:18:15 +0200

    added controller functions

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,12 +1,48 @@
 package Controller;
 
+import Model.Statement.IStatement;
+import Model.States.ExecutionStack.IExecutionStack;
 import Model.States.ProgState;
 import Repository.IRepository;
 
 public class Controller {
     private IRepository repository;
+    private Boolean displayFlag = false;
 
     public Controller(IRepository repository) {
         this.repository = repository;
     }
+
+    public ProgState oneStep(ProgState state) throws AppException {
+        IExecutionStack stack = state.getExecutionStack();
+        if (stack.isEmpty()) {
+            throw new AppException("Program state stack is empty");
+        }
+
+        IStatement currentStatement = stack.pop();
+        return currentStatement.execute(state);
+    }
+
+    public void allSteps() throws AppException {
+        ProgState state = repository.getCurrentProgram();
+        while (!state.getExecutionStack().isEmpty()) {
+            oneStep(state);
+            displayState(state);
+        }
+    }
+
+    public Boolean getDisplayFlag() {
+        return displayFlag;
+    }
+
+    public void setDisplayFlag(Boolean displayFlag) {
+        this.displayFlag = displayFlag;
+    }
+
+    private void displayState(ProgState state) {
+        if(displayFlag) {
+            System.out.println(state);
+        }
+    }
+
 }
